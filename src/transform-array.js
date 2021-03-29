a020702a1f51446a17b407b16856241d98ecf0ba
Fpsska <73740087+Fpsska@users.noreply.github.com>
2021-03-29 23:57:56 +0300

    feat: new solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    } else {
+        let vArr = [];
+
+        for (let j = 0; j < arr.length; j++) {
+            switch (arr[j]) {
+                case '--discard-next':
+                    j++;
+                    break;
+                case '--discard-prev':
+                    (vArr.length && arr[j - 2] != '--discard-next') ? vArr.pop() : vArr;
+                    break;
+                case '--double-next':
+                    (j < arr.length - 1) ? vArr.push(arr[j + 1]) : vArr;
+                    break;
+                case '--double-prev':
+                    (j > 0 && arr[j - 2] != '--discard-next') ? vArr.push(arr[j - 1]) : vArr;
+                    break;
+                default:
+                    vArr.push(arr[j]);
+                    break;
+            }
+        }
+        return vArr;
+    }
+
 };
