384c5d69a093c7d81c892f66b96de02b7521648f
Fpsska <73740087+Fpsska@users.noreply.github.com>
2021-03-29 23:54:55 +0300

    feat: new solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if (Array.isArray(members)) {
+    let names = members.filter(item => typeof item == "string");
+    let sort = names.map(item => item.trim()[0].toUpperCase()).sort();
+
+    return sort.join("");
+
+  } else {
+    return false;
+  }
+
 };
