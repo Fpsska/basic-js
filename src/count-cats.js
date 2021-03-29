0279f62087a0b2d992cf72fd39c97cda38181d28
Fpsska <73740087+Fpsska@users.noreply.github.com>
2021-03-29 23:53:53 +0300

    feat: new solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+
+  const rightSymbol = "^^";
+
+  let newArr = matrix.join().split(',');
+
+  let rightArr = newArr.filter(value => {
+    return value == rightSymbol;
+  });
+
+  let rightArrValue = rightArr.length;
+
+  if (rightArrValue > 0) {
+    return rightArrValue;
+  } else {
+    return 0;
+  }
+
 };
